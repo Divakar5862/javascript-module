d5c27b70c76da9d6801659cf82b3a1b64a58b56c
Glitch (glitch-hello-website) <none>
2023-08-30 11:28:21 +0000

    🌴☺️ Checkpoint ./session6.js:57911820/752

--- session6.js
+++ session6.js
@@ -1,4 +1,4 @@
-var firstname,lastname,age,emailid,number,gender,address;
+var firstname,lastname,age,previous school name,10th mark,12th mark,course details,gender;
 function validation(){
   readForm();
   if(firstname==""){
@@ -10,28 +10,37 @@ function validation(){
     lastname.focus();
     
   }
-  if(emailid==""){
-  alert("please enter your email id");
-  emailid.focus();
+  if(age==""){
+  alert("please enter your age");
+  age.focus();
   }
-  if(number==""){
-    alert("please enter your moblie number");
-    number.focus();
+  if(previous school name==""){
+    alert("please enter your previous school name");
+    previous school name.focus();
     
   }
-  if(address==""){
-    alert("please enter your address");
-    address.focus();
+  if(10th mark==""){
+    alert("please enter your 10th mark");
+    1oth mark.focus();
     
     
   }
+    if(12th mark==""){
+    alert("please enter your 12th mark ");
+    12th mark.focus();
+    }
+    if(course details==""){
+    alert("please enter your course details");
+    course detailsfocus();
+    }
+  
   if(gender==""){
     alert("please enter your gender");
     gender.focus();
     
     
   }
-  console.log(firstname,lastname,emailid,number,address,gender);
+  console.log(firstname,lastname,age,previous school name,10th mark,12th mark,course details,gender);
   
 }
 function readForm(){
