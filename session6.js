5fa0b31d5b38283a69f51d7d0d1d091be74168fb
Glitch (glitch-hello-website) <none>
2023-08-30 11:17:13 +0000

    ⛑🛍 Checkpoint ./session6.css:57911820/2710 ./session6.js:57911820/1443

---
+++ session6.js
@@ -0,0 +1,56 @@
+var firstname,lastname,age,emailid,number,gender,address;
+function validation(){
+  readForm();
+  if(firstname==""){
+    alert("please enter your first name");
+    firstname.focus();
+  }
+  if(lastname==""){
+    alert("please enter your last name");
+    lastname.focus();
+    
+  }
+  if(emailid==""){
+  alert("please enter your email id");
+  emailid.focus();
+  }
+  if(number==""){
+    alert("please enter your moblie number");
+    number.focus();
+    
+  }
+  if(address==""){
+    alert("please enter your address");
+    address.focus();
+    
+    
+  }
+  if(gender==""){
+    alert("please enter your gender");
+    gender.focus();
+    
+    
+  }
+  console.log(firstname,lastname,emailid,number,address,gender);
+  
+}
+function readForm(){
+  firstname=document.getElementById("fname").value;
+  lastname=document.getElementById("lname").value
+  emailid=document.getElementById("mail").value
+  number=document.getElementById("num").value
+  address=document.getElementById("add").value
+  gender=document.getElementById("gen").value
+}
+document.getElementById("submit").onclick=function(){
+  readForm();
+  validation();
+  alert("datasubmit");
+  firstname=document.getElementById("fname").value="";
+  lastname=document.getElementById("lname").value="";
+  emailid=document.getElementById("mail").value="";
+  number=document.getElementById("num").value="";
+  address=document.getElementById("add").value="";
+  gender=document.getElementById("gen").value="";
+  
+}
