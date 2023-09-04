d248858d7af0faf7a5c739b3595c8fc073cb8ba2
Glitch (glitch-hello-website) <none>
2023-09-04 12:53:02 +0000

    🐄🎑 Checkpoint ./session7.js:57911820/585

--- session7.js
+++ session7.js
@@ -1,3 +1,27 @@
 function add(){
-  var a=par
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a+b;
+  document.form.op.value=c;
+}
+function sub(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a-b;
+  document.form.op.value=c;
+  
+}
+function mul(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a*b;
+  document.form.op.value=c;
+  
+  
+}
+function div(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseint(document.form.sn.value);
+  var c=a
+  
 }
