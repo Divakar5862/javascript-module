d968a6ff7919aa505f734970135fd7fb16712a73
Glitch (glitch-hello-website) <none>
2023-09-09 07:51:38 +0000

    😚🍀 Checkpoint ./task8.html:57911820/440 ./session7.js:57911820/34 ./task8.js:57911820/1244

---
+++ task8.js
@@ -0,0 +1,61 @@
+function add(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a+b;
+  document.form.op.value=c;
+}
+function sub(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a-b;
+  document.form.op.value=c;
+  
+}
+function mul(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a*b;
+  document.form.op.value=c;
+  
+  
+}
+function div(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a/b;
+  document.form.op.value=c;
+  
+  
+}
+function greater(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a>b;
+  document.form.op.value=c;
+  
+  
+}
+function lesser(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a<b;
+  document.form.op.value=c;
+  
+  
+}
+function greatequalto(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a*b;
+  document.form.op.value=c;
+  
+  
+}
+function lesserequalto(){
+  var a=parseInt(document.form.fn.value);
+  var b=parseInt(document.form.sn.value);
+  var c=a/b;
+  document.form.op.value=c;
+  
+  
+}
