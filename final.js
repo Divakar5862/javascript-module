a41e622faeb9ba57159a10dae18b970d34a30625
Glitch (glitch-hello-website) <none>
2023-09-30 06:29:54 +0000

    🏫🍨 Checkpoint ./final.js:57911820/500

--- final.js
+++ final.js
@@ -11,14 +11,3 @@ signInLink.addEventListener('click', () => {
     wrapper.classList.add('animated-signup');
     wrapper.classList.remove('animated-signin');
 });
-document.getElementById("").onclick=function(){
-  readForm();
-  validation();
-  alert("datasubmit");
-  username=document.getElementById("form-group").value="";
-  Email=document.getElementById("form-group").value="";
-  password=document.getElementById("form-group").value="";
-  con=document.getElementById("form-group").value="";
-  address=document.getElementById("form-group").value="";
-  gender=document.getElementById("form-group").value="";
-  
